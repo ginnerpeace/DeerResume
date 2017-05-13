6ec9809ed2adbb52f0721d587b5d461820444aae
gjy <ginnerpeace@live.com>
2017-05-13 19:38:22 +0800

    修改为本地版本

--- data.php
+++ data.php
@@ -1,30 +1,26 @@
 <?php
 
-$viewpass = '1234'; // 密码必须大于3位。留空则任何人可以访问
-$title = '标题';
-$subtitle = '子标题';
-$content = 'MarkDown';
-
-$data['local'] = 1;
-
-
-if( strlen( $viewpass ) > 0 && trim($_REQUEST['vpass']) != $viewpass )
-{
-	$data['errno'] = '0';
-	$data['show'] = 0;
-	$data['title'] = '';
-	$data['subtitle'] = '';
-	$data['content'] = '';
-}
-else
-{
-	
-	$data['errno'] = '0';
-	$data['show'] = 1;
-	$data['title'] = $title;
-	$data['subtitle'] = $subtitle;
-	$data['content'] = $content;
-
+$localfile = __DIR__ . '/data/resume.md';
+if (! file_exists($localfile)) {
+    jsonResponse([
+        'local' => 1,
+        'errno' => 1,
+        'show' => 0,
+        'title' => '',
+        'subtitle' => '',
+        'content' => '',
+    ]);
 }
 
-echo json_encode( $data );
+jsonResponse([
+    'errno' => 0,
+    'show' => 1,
+    'title' => '苟锦阳的简历',
+    'subtitle' => 'PHP开发',
+    'content' => file_get_contents($localfile),
+]);
+
+function jsonResponse(array $data) {
+    header('Content-type: application/json');
+    exit(json_encode($data, JSON_UNESCAPED_UNICODE));
+}
